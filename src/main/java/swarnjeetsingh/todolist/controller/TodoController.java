3221091d1e4c8a5dcd22eff4fae9fdb8f1953b71
Swarnjeet Singh <swarnjeet7@gmail.com>
2025-07-20 00:19:18 +0530

    Optional Added

--- src/main/java/swarnjeetsingh/todolist/controller/TodoController.java
+++ src/main/java/swarnjeetsingh/todolist/controller/TodoController.java
@@ -1,5 +1,8 @@
 package swarnjeetsingh.todolist.controller;
 
+import jakarta.persistence.PostUpdate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import swarnjeetsingh.todolist.model.TodoModel;
 import swarnjeetsingh.todolist.service.TodoService;
@@ -7,6 +10,7 @@ import swarnjeetsingh.todolist.service.TodoService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todos")
@@ -17,23 +21,48 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    @GetMapping
+    @GetMapping("/getAll")
     public List<TodoModel> getAllTodos() {
         return todoService.getAllTodos();
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public TodoModel addTodo(@RequestBody TodoModel todo) {
         return todoService.addTodo(todo);
     }
 
     @GetMapping("getById/{id}")
-    public TodoModel getTodo(@PathVariable int id) {
+    public Optional<TodoModel> getTodo(@PathVariable int id) {
         return todoService.getTodoById(id);
     }
 
-    @DeleteMapping("{id}")
-    public TodoModel deleteTodo(@PathVariable int id) {
-        return todoService.deleteTodoById(id);
+    @DeleteMapping("deleteById/{id}")
+    public Optional<TodoModel> deleteTodo(@PathVariable int id) {
+        Optional<TodoModel> todo = todoService.deleteTodoById(id);
+
+        if(todo.isPresent()) {
+            TodoModel deletedTodo = todo.get();
+            return Optional.of(deletedTodo);
+        }
+        return Optional.empty();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateTodo(@RequestBody TodoModel updatedTodo) {
+        Optional<TodoModel> optionalTodo = todoService.getTodoById(updatedTodo.getId());
+
+        if (optionalTodo.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Todo not found");
+        }
+
+        TodoModel existingTodo = optionalTodo.get();
+        existingTodo.setTask(updatedTodo.getTask());
+        existingTodo.setDescription(updatedTodo.getDescription());
+        existingTodo.setCompleted(updatedTodo.isCompleted());
+        existingTodo.setActive(updatedTodo.isActive());
+
+        todoService.addTodo(existingTodo);
+
+        return ResponseEntity.ok(existingTodo);
     }
 }
--- src/main/java/swarnjeetsingh/todolist/service/TodoService.java
+++ src/main/java/swarnjeetsingh/todolist/service/TodoService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -30,18 +31,23 @@ public class TodoService {
         return todosRepository.save(todoModel);
     }
 
-    public TodoModel getTodoById(int id) {
+    public Optional<TodoModel> getTodoById(int id) {
         logger.info("Retrieving todo with id: {}", id);
-        return todosRepository.findById(id);
+        return Optional.ofNullable(todosRepository.findById(id));
     }
 
-    public TodoModel deleteTodoById(int id) {
+    public Optional<TodoModel> deleteTodoById(int id) {
         logger.info("Deleting todo with id: {}", id);
-        TodoModel todo = todosRepository.findById(id);
-        todo.setActive(false);
-        todosRepository.save(todo);
-        logger.info("DeActivated todo with id: {}", id);
-        return todo;
+        Optional<TodoModel> optionalTodo = Optional.ofNullable(todosRepository.findById(id));
+
+        if(optionalTodo.isPresent()) {
+            TodoModel todo = optionalTodo.get();
+            todo.setActive(false);
+            todosRepository.save(todo);
+            logger.info("DeActivated todo with id: {}", id);
+            return Optional.of(todo);
+        }
+        return Optional.empty();
     }
 
 }
